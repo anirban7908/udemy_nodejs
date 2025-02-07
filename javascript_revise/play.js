b66af8d0541f6848fecbbac23db37a203c604d83
Anirban <anirban.choudhury.7908@gmail.com>
2025-02-07 17:30:26 +0530

    first commit

---
+++ javascript_revise/play.js
@@ -0,0 +1,14 @@
+let user_name = 'Anirban'; // Use let for variables that change
+console.log(user_name);
+
+
+const person = {
+    name : 'Anirban',
+    age: 29,
+    greet(){
+        console.log(`hello Testing`);
+        
+    }
+}
+person.greet();
+
