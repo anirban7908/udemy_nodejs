bbbfc613ecaf490b3a50908477de6e7bc4282612
Anirban <anirban.choudhury.7908@gmail.com>
2025-06-25 17:09:37 +0530

    Initial commit

---
+++ practice/app.js
@@ -0,0 +1,16 @@
+// step1: create new server
+const http = require('http');
+
+const server = http.createServer((req, res)=>{
+
+    const url = req.url;
+    if(url == '/'){
+
+    };
+    if(url == '/users'){
+         
+    };
+});
+
+server.listen(3000);
+
